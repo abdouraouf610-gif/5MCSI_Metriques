da7a3963a5ebc8b7a897ab74cfb804239348a1eb
abdouraouf610-gif <abdouraouf610@gmail.com>
2025-12-02 12:28:00 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -12,8 +12,9 @@ def hello_world():
     return render_template('hello.html')
   
 @app.route("/contact/")
-def MaPremiereAPI():
-    return "<h2>Ma page de contact</h2>"  
+def contact():
+    return "<h2>Ma page de contact</h2>"
+
 @app.route('/tawarano/')
 def meteo():
     response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
@@ -25,11 +26,14 @@ def meteo():
         temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
+
 @app.route("/rapport/")
 def mongraphique():
-    return render_template("graphique.html")  
+    return render_template("graphique.html")
+
 @app.route("/histogramme/")
-def mongraphique():
-    return render_template("histogramme.html")  
+def histogramme():
+    return render_template("histogramme.html")
+  
 if __name__ == "__main__":
   app.run(debug=True)
